07583b619f18e4be08058c5fd4362cebb9760ab7
comestime <comestime@gmail.com>
2017-07-26 23:04:16 -0700

    add dfs packages

---
+++ src/dfs/SubsetWithDup.java
@@ -0,0 +1,73 @@
+/*
+Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+
+Note: The solution set must not contain duplicate subsets.
+
+For example,
+If nums = [1,2,2], a solution is:
+
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+ */
+
+package dfs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SubsetWithDup {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (nums == null) return result;
+        if (nums.length == 0) {
+        	result.add(new ArrayList<Integer>());
+        	return result;
+        }
+        
+        // map to track the number of occurrence of each element
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+        	if (map.containsKey(nums[i])) {
+        		map.put(nums[i], map.get(nums[i]) + 1);
+        	} else {
+        		map.put(nums[i], 1);
+        	}
+        }
+        List<Integer> distinctNum = new ArrayList<Integer>(map.keySet());
+        List<Integer> temp = new ArrayList<Integer>();
+        dfs(map, distinctNum, 0, temp, result);
+        return result;
+    }
+    
+    private void dfs(Map<Integer, Integer> map, List<Integer> distinctNum, int index, List<Integer> temp, List<List<Integer>> result) {
+    	if (index == distinctNum.size()) {
+    		result.add(new ArrayList<Integer>(temp));
+    		return;
+    	}
+    	
+    	int cur = distinctNum.get(index);
+    	for (int i = 0; i <= map.get(cur); i++) {
+    		for (int j = 0; j < i; j++) {
+    			temp.add(cur);
+    		}
+    		dfs(map, distinctNum, index + 1, temp, result);
+    		for (int j = 0; j < i; j++) {
+    			temp.remove(temp.size() - 1);
+    		}
+    	}
+    }
+    
+    public static void main(String[] args) {
+    	SubsetWithDup solution = new SubsetWithDup();
+    	int[] test = new int[] {1, 2, 2};
+    	System.out.println(solution.subsetsWithDup(test));
+    }
+}
